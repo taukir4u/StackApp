e2faf4cb73889be06cca714815878a3c7db16229
taukir4u <taukir4u@gmail.com>
2014-12-26 20:52:20 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ StackApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> nameStack = new Stack<string>();
+            nameStack.Push("Taukir");
+            nameStack.Push("Hasan");
+            nameStack.Push("Topu");
+            nameStack.Push("Aysa");
+            nameStack.Push("Siddika");
+            nameStack.Push("Tondra");
+
+            Console.WriteLine("Pushing Names into Stack: ");
+            foreach (var aName in nameStack)
+            {
+                Console.WriteLine(aName); 
+            }
+
+            Console.WriteLine("Total Value: " + nameStack.Count);
+            Console.WriteLine();
+
+
+            Console.WriteLine("Popping Names from Stack: ");
+            int count = nameStack.Count;
+
+            for (var i = 0; i < count; i++)
+            {
+                Console.WriteLine(nameStack.Pop());
+            }
+            
+            Console.WriteLine("Total Value: " + nameStack.Count);
+            Console.ReadKey();
+        }
+    }
+}
